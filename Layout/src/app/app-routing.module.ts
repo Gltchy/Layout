06a460544c07893df74a6eabfd8a39e87089a84f
Gltchy <ahmed.lashheb@gmail.com>
2020-12-11 22:38:41 +0100

    fix

--- Layout/src/app/app-routing.module.ts
+++ Layout/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
@@ -34,7 +35,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
 })
 export class AppRoutingModule { }
